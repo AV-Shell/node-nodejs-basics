cd68bc0f7ac8d08f005163603c1f5935879faa8a
AV-Shell <avshell.sj@gmail.com>
2022-12-05 20:58:39 +0300

    feat: add streams solutions

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,22 @@
+import { createReadStream } from 'node:fs';
+import { stat } from 'node:fs/promises';
+import { getPath } from '../common/utils.js'
+import { fsErrorTest } from '../common/consts.js';
+
 const read = async () => {
-    // Write your code here 
+    const filename = getPath(import.meta.url, './files/fileToRead.txt');
+    try {
+        const isInputFile = await stat(filename).then(x => x.isFile());
+
+        if (!isInputFile) {
+            throw new Error(fsErrorTest);
+        }
+
+        createReadStream(filename).pipe(process.stdout).on('error', () => { throw new Error(fsErrorTest); })
+
+    } catch (error) {
+        throw new Error(fsErrorTest);
+    }
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,24 @@
+import { createWriteStream } from 'node:fs';
+import { stat } from 'node:fs/promises';
+import { getPath } from '../common/utils.js'
+import { fsErrorTest } from '../common/consts.js';
+
 const write = async () => {
-    // Write your code here 
+    const filepath = getPath(import.meta.url, './files/fileToWrite.txt');
+    try {
+        const isInputFile = await stat(filepath).then(x => x.isFile());
+
+        if (!isInputFile) {
+            throw new Error(fsErrorTest);
+        }
+
+        const writeStream = createWriteStream(filepath);
+
+        process.stdin.pipe(writeStream).on('error', () => { throw new Error(fsErrorTest); })
+
+    } catch (error) {
+        throw new Error(fsErrorTest);
+    }
 };
 
 await write();
