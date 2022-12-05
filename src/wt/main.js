c133911f1219d1caa3b6ef6dd5e05d0b2f0999da
AV-Shell <avshell.sj@gmail.com>
2022-12-05 23:10:07 +0300

    feat: add workers solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+import { getPath } from '../common/utils.js';
+
+
+
+const workerPromise = (filename, data) => {
+    return new Promise((resolve, reject) => {
+        const worker = new Worker(filename, {
+            workerData: data
+        });
+        worker.on('message', resolve);
+        worker.on('error', reject);
+        worker.on('exit', (code) => {
+            if (code !== 0)
+                reject(new Error(`Worker stopped with exit code ${code}`));
+        });
+    });
+};
+
 const performCalculations = async () => {
-    // Write your code here
+    const workerFileName = getPath(import.meta.url, './worker.js');
+    const workersResult = await Promise
+        .allSettled(cpus().map((x, i) => workerPromise(workerFileName, 10 + i)))
+        .then(y => y.map((r) => r.status === 'fulfilled' ? { status: 'resolved', data: r.value } : { status: 'error', data: null }));
+
+    console.log(workersResult)
+
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,21 @@
+import { workerData, parentPort } from 'worker_threads';
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    // uncomment below if you want to check with fallen threads
+
+/* 
+    if (Math.random() > 0.5) {
+        throw new Error();
+    } else {
+        parentPort.postMessage(nthFibonacci(workerData))
+    }
+    return;
+ */
+
+    parentPort.postMessage(nthFibonacci(workerData))
+
 };
 
 sendResult();
