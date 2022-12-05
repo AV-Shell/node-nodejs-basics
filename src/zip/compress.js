8efb7d43eb9b8411640bf70ff98eaeccf300dc6b
AV-Shell <avshell.sj@gmail.com>
2022-12-05 21:34:23 +0300

    feat: add zip solutions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,36 @@
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { stat } from 'node:fs/promises';
+import { getPath } from '../common/utils.js'
+import { fsErrorTest } from '../common/consts.js';
+
 const compress = async () => {
-    // Write your code here 
-};
+    const inputFileName = getPath(import.meta.url, './files/fileToCompress.txt');
+    const outputFileName = getPath(import.meta.url, './files/archive.gz');
+    try {
+        const isInputFile = await stat(inputFileName).then(x => x.isFile());
+
+        if (!isInputFile) {
+            throw new Error(fsErrorTest);
+        }
+
+
+
+        const gzip = createGzip();
+        const source = createReadStream(inputFileName);
+        const destination = createWriteStream(outputFileName);
+
+        pipeline(source, gzip, destination, (err) => {
+            if (err) {
+                throw new Error(fsErrorTest);
+            }
+        });
+
+    } catch (error) {
+        throw new Error(fsErrorTest);
+    }
+
+}
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,33 @@
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { stat } from 'node:fs/promises';
+import { getPath } from '../common/utils.js'
+import { fsErrorTest } from '../common/consts.js';
+
 const decompress = async () => {
-    // Write your code here 
+    const inputFileName = getPath(import.meta.url, './files/archive.gz');
+    const outputFileName = getPath(import.meta.url, './files/fileToCompress.txt');
+    try {
+        const isInputFile = await stat(inputFileName).then(x => x.isFile());
+
+        if (!isInputFile) {
+            throw new Error(fsErrorTest);
+        }
+
+        const gzip = createGunzip();
+        const source = createReadStream(inputFileName);
+        const destination = createWriteStream(outputFileName);
+
+        pipeline(source, gzip, destination, (err) => {
+            if (err) {
+                throw new Error(fsErrorTest);
+            }
+        });
+
+    } catch (error) {
+        throw new Error(fsErrorTest);
+    }
 };
 
 await decompress();
